9694412eb19171f2917a25c7989952818f4c1fe8
Rommel Jay San Jose <arjae.sj@gmail.com>
2020-08-17 19:18:11 +0800

    modified source code to add styling

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,3 +1,5 @@
+/* eslint-disable indent */
+/* eslint-disable prettier/prettier */
 // Requiring path to so we can use relative routes to our HTML files
 // const path = require("path");
 
@@ -7,34 +9,34 @@ const isAuthenticated = require("../config/middleware/isAuthenticated");
 const ru = require("./routesUtil");
 
 module.exports = function(app) {
-  app.get("/", (req, res) => {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      // res.redirect("/members");
-      res.render("recipes");
-    }
-    // res.sendFile(path.join(__dirname, "../public/signup.html"));
-    res.render("signup");
-  });
+    app.get("/", (req, res) => {
+        // If the user already has an account send them to the members page
+        if (req.user) {
+            // res.redirect("/members");
+            res.render("recipes");
+        }
+        // res.sendFile(path.join(__dirname, "../public/signup.html"));
+        res.render("signup-login");
+    });
 
-  app.get("/login", (req, res) => {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      // res.redirect("/members");
-      res.render("recipes");
-    }
-    // res.sendFile(path.join(__dirname, "../public/login.html"));
-    res.render("login");
-  });
+    app.get("/signup-login", (req, res) => {
+        // If the user already has an account send them to the members page
+        if (req.user) {
+            // res.redirect("/members");
+            res.render("recipes");
+        }
+        // res.sendFile(path.join(__dirname, "../public/login.html"));
+        res.render("signup-login");
+    });
 
-  app.get("/recipes-home-page", isAuthenticated, async (request, response) => {
-    response.render("recipes");
-  });
+    app.get("/recipes-home-page", isAuthenticated, async(request, response) => {
+        response.render("recipes");
+    });
 
-  // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/my-recipes", isAuthenticated, async (request, response) => {
-    const recipeData = await ru.getAllRecipesForCurrentUser(request);
-    response.render("recipes", { recipe: recipeData });
-  });
+    // Here we've add our isAuthenticated middleware to this route.
+    // If a user who is not logged in tries to access this route they will be redirected to the signup page
+    app.get("/my-recipes", isAuthenticated, async(request, response) => {
+        const recipeData = await ru.getAllRecipesForCurrentUser(request);
+        response.render("recipes", { recipe: recipeData });
+    });
 };
