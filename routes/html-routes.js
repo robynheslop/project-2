49785baca1f1c3567c3b2d7be69df4840cc2dea5
Nitin Mukesh <nitinmukes@gmail.com>
2020-08-20 02:10:06 +0800

    MR10.10_Fixed_Field_Level_Validations_Correct_Form_Names

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,40 +1,30 @@
-/* eslint-disable indent */
-/* eslint-disable prettier/prettier */
-// Requiring path to so we can use relative routes to our HTML files
-// const path = require("path");
-
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 // require to create/get/update recipes
 const ru = require("./routesUtil");
 
 module.exports = function(app) {
-    app.get("/", (req, res) => {
-        // If the user already has an account send them to the members page
-        if (req.user) {
-            // res.redirect("/members");
-            res.render("recipes");
-        }
-        // res.sendFile(path.join(__dirname, "../public/signup.html"));
-        res.render("signup-login");
-    });
+  app.get("/", (req, res) => {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.render("recipesHome");
+    }
+    res.render("signup-login");
+  });
 
-    app.get("/signup-login", (req, res) => {
-        // If the user already has an account send them to the members page
-        if (req.user) {
-            // res.redirect("/members");
-            res.render("recipes");
-        }
-        // res.sendFile(path.join(__dirname, "../public/login.html"));
-        res.render("signup-login");
-    });
-
+  app.get("/signup-login", (req, res) => {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.render("recipesHome");
+    }
+    res.render("signup-login");
+  });
 
   app.get("/recipes-home-page", isAuthenticated, (request, response) => {
     response.render("recipesHome");
   });
 
-   // new html route to render new handlebar 
+  // new html route to render new handlebar
   app.get("/add-recipe", isAuthenticated, (request, response) => {
     response.render("newRecipeForm");
   });
@@ -44,10 +34,11 @@ module.exports = function(app) {
   app.get("/my-recipes", isAuthenticated, async (request, response) => {
     const recipeData = await ru.getAllRecipesForCurrentUser(request);
     if (recipeData && !recipeData.length) {
-      const responseString = "You have no recipes saved! Start adding them today.";
-      response.render("noSearchResults", {noResults: responseString});
+      const responseString =
+        "You have no recipes saved! Start adding them today.";
+      response.render("noSearchResults", { noResults: responseString });
     } else {
-      response.render("listRecipes", { recipe: recipeData });    
+      response.render("listRecipes", { recipe: recipeData });
     }
   });
 
@@ -61,10 +52,9 @@ module.exports = function(app) {
     const recipeData = await ru.getRecipesByTextSearch(request);
     if (recipeData && !recipeData.length) {
       const responseString = `We can't find any recipes containing \"${request.query.searchText}\". Let's look for something else...`;
-      response.render("noSearchResults", {noResults: responseString});
+      response.render("noSearchResults", { noResults: responseString });
     } else {
-      response.render("listRecipes", {recipe: recipeData});    
+      response.render("listRecipes", { recipe: recipeData });
     }
   });
 };
-
