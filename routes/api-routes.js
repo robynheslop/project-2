57ce289763248b795c4b0338df5305c8390db6fd
Nitin Mukesh <nitinmukes@gmail.com>
2020-08-22 21:45:23 +0800

    Removing Merge errros

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -110,13 +110,10 @@ module.exports = function(app) {
       })
       .catch(error => console.log("Error", error));
   });
-<<<<<<< HEAD
 
   app.get("/recipe-of-the-day", async (request, response) => {
     const recipeDetails = await ru.getAllRecipeIds();
     const selectedID = Math.floor(Math.random() * recipeDetails.length);
     response.json(recipeDetails[selectedID]);
   });
-=======
->>>>>>> 05921de... pushing branch up
 };
